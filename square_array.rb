8643f9e7c798690d3d48d2e2554bd1f70ee21035
Arthur Torres <arthurtorres002@gmail.com>
2018-11-19 20:12:21 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,10 @@
 def square_array(array)
-  index = 0
-  array.each do |element|
-    array[index] = element ** 2
-    index += 1
-  end 
+  
+  #index = 0
+  #array.each do |element|
+  #  array[index] = element ** 2
+  #  index += 1
+  #end 
+  
+  array.collect {|x| x ** 2}
 end
